46db7e4293d9c2c50326517d259e3c905a427eda
zhangjinyuan <zhangjinyuan@huajiao.tv>
2019-03-25 11:59:28 +0800

    zjytest

---
+++ zjytest/ios/Classes/ZjytestPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface ZjytestPlugin : NSObject<FlutterPlugin>
+@end
---
+++ zjytest/ios/Classes/ZjytestPlugin.m
@@ -0,0 +1,20 @@
+#import "ZjytestPlugin.h"
+
+@implementation ZjytestPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"zjytest"
+            binaryMessenger:[registrar messenger]];
+  ZjytestPlugin* instance = [[ZjytestPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
